df6afc1f35db94f2c1b217cfc135d711e999855a
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-27 16:55:50 +0300

    using functions locally to reach data

--- pages/guides.js
+++ pages/guides.js
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
 import styles from '../styles/Guides.module.css'
 
 export default function Guides() {
+  
+  //now gonna fetch the info from the fucntion locally
+  useEffect(() => {
+    fetch('/.netlify/functions/supermario')
+      .then(res => res.json())
+      .then(data => console.log("data ==>", data))
+  },[])
+  
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
